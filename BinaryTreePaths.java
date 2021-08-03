8c265481de9e4cff9637c0a814661172b7163164
Jashay Lilani <jashay.lilani@gmail.com>
2021-08-02 20:02:49 -0400

    Binary Tree Paths

---
+++ BinaryTreePaths.java
@@ -0,0 +1,28 @@
+public class BinaryTreePaths {
+  public void dfs(TreeNode root, String current, List<String> output) {
+        
+    if (root == null) {
+        return;
+    }
+    
+    if (root.left == null && root.right == null) {
+        current+=String.valueOf(root.val);
+        output.add(current.toString());
+        return;
+    }
+    
+    current += String.valueOf(root.val)+"->";
+    
+    dfs(root.left, current, output);
+    dfs(root.right, current, output);
+}
+
+  public List<String> binaryTreePaths(TreeNode root) {
+      
+      List<String> output = new ArrayList<>();
+      
+      dfs(root, "", output);
+      
+      return output;
+  }
+}
