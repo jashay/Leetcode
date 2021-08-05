0e722a8dd89f93776403d1491bf8575c6865fb93
Jashay Lilani <jashay.lilani@gmail.com>
2021-08-05 15:00:23 -0400

    MinDepth

--- HappyNumber.java
+++ HappyNumber.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class HappyNumber {
   public boolean isHappy(int n) {
     Set<Integer> set = new HashSet<>();
---
+++ MinDepthOfBinartTree.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class MinDepthOfBinartTree {
+  private void dfs(TreeNode root, int currDepth, int[] minDepth) {
+        
+    if (root.left == null && root.right == null) {
+       minDepth[0] = Math.min(currDepth+1, minDepth[0]);
+       return; 
+    }
+    
+    if (root.left != null) {
+        dfs(root.left, currDepth+1, minDepth);
+    }
+    
+    if (root.right != null) {
+        dfs(root.right, currDepth+1, minDepth);
+    }   
+}
+
+  public int minDepth(TreeNode root) {
+      
+      int[] minDepth = new int[1];
+      
+      minDepth[0] = Integer.MAX_VALUE;
+      
+      if (root != null) {
+          dfs(root, 0, minDepth);
+          return minDepth[0];
+      }
+      else return 0;
+  }
+}
