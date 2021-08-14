bf01cf8b28261a59ce9d50a44ba563e6552214a1
Jashay Lilani <jashay.lilani@gmail.com>
2021-08-13 22:04:53 -0400

    J&S

--- JewelsStones.java
+++ JewelsStones.java
@@ -1,16 +1,18 @@
 import java.util.*;
+
 public class JewelsStones {
   Set<Character> set = new HashSet<>();
         
-        for (char c : jewels.toCharArray()) {
-            set.add(c); 
-        }
-        int count = 0;
-        for (char c : stones.toCharArray()) {
-            if (set.contains(c)) {
-                count++;
-            }
-        }
+  for (char x : jewels.toCharArray()) {
+      set.add(x); 
+  }
+  int count = 0;
+  
+  for (char c : stones.toCharArray()) {
+      if (set.contains(c)) {
+          count++;
+      }
+  }
         
-        return count;
+  return count;
 }
