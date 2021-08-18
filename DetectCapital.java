8f57ada75205a27dc1b523527001bca05400d724
Jashay Lilani <jashay.lilani@gmail.com>
2021-08-18 14:40:42 -0400

    Detecting capital

---
+++ DetectCapital.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class DetectCapital {
+  public boolean detectCapitalUse(String word) {
+    int count = 0;
+    for (char c :word.toCharArray()) {
+        if (!Character.isUpperCase(c)) {
+            continue;
+        } else {
+            count++;
+        }
+    }
+    
+    
+    if (count == 1 && Character.isUpperCase(word.charAt(0))) return true;
+    else if (count == 0) return true;
+    else if (count == word.length()) return true;
+    else return false;
+}
+}
