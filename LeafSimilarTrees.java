e0803b08b31f8b9a19ef07ff69847ce686e2d19b
Jashay Lilani <jashay.lilani@gmail.com>
2021-08-20 01:16:45 -0400

    Leaf Similar trees

---
+++ LeafSimilarTrees.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class LeafSimilarTrees {
+  public void dfs(TreeNode root, List<Integer> list) {
+    if (root == null) return;
+    
+    if (root.left == null && root.right == null) {
+        list.add(root.val);
+        return;
+    }
+    
+    dfs(root.left, list);
+    dfs(root.right, list);
+}
+    
+
+public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+    List<Integer> list1 = new ArrayList<>();
+    List<Integer> list2 = new ArrayList<>();
+    dfs(root1, list1);
+    dfs(root2, list2);
+    
+    if (list1.size() != list2.size()) return false;
+    
+    for (int i=0; i<list1.size(); i++) {
+        if (list1.get(i) != list2.get(i)) return false;
+    }
+    
+    return true;
+}
+}
