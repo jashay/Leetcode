1db634dfedf0be8b1b2ff623b045725ba2b3014c
Jashay Lilani <jashay.lilani@gmail.com>
2021-08-24 17:06:26 -0400

    Insert into BST

---
+++ InsertIntoBST.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class InsertIntoBST {
+  public TreeNode insertIntoBST(TreeNode root, int val) {
+    TreeNode curr = root;
+
+    if (curr == null) return new TreeNode(val);
+    
+    while(true){
+        if(val <= curr.val){
+            if(curr.left != null){
+                curr = curr.left;
+            }
+            else {
+                curr.left = new TreeNode(val);
+                break;
+            }
+        }
+        if(val >= curr.val){
+            if(curr.right != null){
+                curr = curr.right;
+            }
+            else {
+                curr.right = new TreeNode(val);
+                break;
+            }
+        }
+    }
+    return root;
+  }
+}
