127f04659557ad28b8a2d8a74db4252a4577fff0
Jashay Lilani <jashay.lilani@gmail.com>
2021-08-26 19:12:45 -0400

    Changes

--- JewelsStones.java
+++ JewelsStones.java
@@ -1,20 +1,23 @@
 import java.util.*;
 
 public class JewelsStones {
-  Set<Character> set = new HashSet<>();
+
+    public int jewelsStones(String jewels, String stones) {
+        Set<Character> set = new HashSet<>();
+                
+        for (char x : jewels.toCharArray()) {
+            set.add(x); 
+        }
         
-  for (char x : jewels.toCharArray()) {
-      set.add(x); 
-  }
-  
-  
-  int count = 0;
-  
-  for (char c : stones.toCharArray()) {
-      if (set.contains(c)) {
-          count++;
-      }
-  }
         
-  return count;
+        int count = 0;
+        
+        for (char c : stones.toCharArray()) {
+            if (set.contains(c)) {
+                count++;
+            }
+        }
+                
+        return count;
+    }
 }
