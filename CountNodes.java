cd8884ad1fa4306b04a1077fe5efa11389486823
Jashay Lilani <jashay.lilani@gmail.com>
2021-08-28 13:18:50 -0400

    count nodes!

---
+++ CountNodes.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class CountNodes {
+  public int countNodes(TreeNode root) {
+        
+    if (root == null) return 0;
+    
+    Queue<TreeNode> q = new LinkedList<>();
+    q.add(root);
+    int count = 0;
+    
+    while (!q.isEmpty()) {
+        int size = q.size();
+        TreeNode curr = q.poll();
+        count++;
+        if (curr.left != null) q.add(curr.left);
+        if (curr.right != null) q.add(curr.right);
+    }
+    
+    return count;
+}
+}
