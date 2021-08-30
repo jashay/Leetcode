9a236250e764c00b8ea0384eb1abf797437705fc
Jashay Lilani <jashay.lilani@gmail.com>
2021-08-29 22:47:31 -0400

    preorder traversal

---
+++ PreOrderTraversal.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class PreOrderTraversal {
+  public void preorder(TreeNode root, List<Integer> ans) {
+    if (root == null) return;
+    
+    ans.add(root.val);
+    preorder(root.left, ans);
+    preorder(root.right, ans);
+  }
+
+  public List<Integer> preorderTraversal(TreeNode root) {
+      List<Integer> ans = new ArrayList<>();
+      
+      preorder(root, ans);
+      
+      return ans;
+  }
+}
