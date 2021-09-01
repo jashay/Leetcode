44372d3d60bb31d2157f5540e66ae68de1dfe252
Jashay Lilani <jashay.lilani@gmail.com>
2021-09-01 10:39:05 -0400

    Can Construct

---
+++ CanConstruct.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class CanConstruct {
+  public boolean canConstruct(String ransomNote, String magazine) {
+    Map<Character, Integer> map = new HashMap<>();
+    
+    for (char c : magazine.toCharArray()) {
+        map.put(c, map.getOrDefault(c, 0)+1);
+    }
+    
+    for (char c : ransomNote.toCharArray()) {
+        if (map.containsKey(c)) {
+            map.put(c, map.get(c)-1);
+            if (map.get(c) == 0) {
+                map.remove(c);
+            }
+        }
+        else {
+            return false;
+        }
+    }
+    
+    return true;
+  }
+}
