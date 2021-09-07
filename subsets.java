d8b391518597a7dc0a47ed7ef8885a3315cd12af
Jashay Lilani <jashay.lilani@gmail.com>
2021-09-07 15:52:15 -0400

    Subsets

---
+++ subsets.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class subsets {
+  public void recursivePut(int[] nums, int i, Set<String> set, String curr) {
+        
+    set.add(curr);
+    
+    if (i >= nums.length) return;
+    
+    if (!set.contains(curr+nums[i])) recursivePut(nums, i+1, set, curr+nums[i]+",");
+    recursivePut(nums, i+1, set, curr);
+}
+
+public List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> ans = new ArrayList<>();
+    ans.add(new ArrayList<>());
+    if (nums.length == 0) return ans;
+    
+    Set<String> set = new HashSet<>();
+    
+    recursivePut(nums, 0, set, "");
+    
+    for (String x : set) {
+        if (!x.equals("")) {
+            String[] array = x.split(",");
+            List<Integer> list = new ArrayList<>();
+            for (String str : array) {
+                list.add(Integer.parseInt(str));
+            }
+            ans.add(list);
+        }
+    }
+    
+    return ans;
+}
+}
