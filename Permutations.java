4680644e8b9125568f83baf6b10423bb167b52f2
Jashay Lilani <jashay.lilani@gmail.com>
2021-09-09 14:43:49 -0400

    Permuatations

---
+++ Permutations.java
@@ -0,0 +1,23 @@
+class Permutations {
+  public void recursive(int[] nums, int start, List<Integer> curr, List<List<Integer>> ans) {
+        
+    if (curr.size() == nums.length) {
+        ans.add(new ArrayList<>(curr));
+    }
+    for (int i=0; i<nums.length; i++) {
+        if (curr.contains(nums[i])) continue;
+        curr.add(nums[i]);
+        recursive(nums, i+1, curr, ans);
+        curr.remove(curr.size()-1);
+    }
+}
+
+  public List<List<Integer>> permute(int[] nums) {
+      
+      List<List<Integer>> ans = new ArrayList<>();
+      
+      recursive(nums, 0, new ArrayList<>(), ans);
+      
+      return ans;
+  }
+}
