2bc2626fb679ffdda3370b8bba974d8c5ef84533
Jashay Lilani <jashay.lilani@gmail.com>
2021-09-18 18:08:49 -0400

    High Five

---
+++ HighFive.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class HighFive {
+  public int[][] highFive(int[][] items) {
+    Map<Integer, List<Integer>> map = new LinkedHashMap();
+    
+    for (int[] i : items) {
+        List<Integer> list;
+        if (map.containsKey(i[0])) {
+            list = map.get(i[0]);
+        }
+        else {
+            list = new ArrayList();
+        }
+        list.add(i[1]);
+        map.put(i[0], list);
+    }
+    List<int[]> res = new ArrayList();
+    
+    for (int key : map.keySet()) {
+        List<Integer> li = map.get(key);
+        Collections.sort(li, (a,b) -> b-a);
+        
+        int sum = 0;
+        for (int i=0; i<5; i++) {
+            sum+=li.get(i);
+        }
+        int avg = sum/5;
+        
+        res.add(new int[]{key, avg});
+    }
+    
+    int[][] ans=new int[res.size()][2];
+    
+    for (int i=0; i<res.size(); i++) {
+        ans[i] = res.get(i);
+    }
+    
+    Arrays.sort(ans, (a,b) -> a[0] - b[0]);
+    
+    return ans;
+    
+  }
+}
