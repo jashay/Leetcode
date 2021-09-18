d691603f75c1d3094eac4660a7f486e4b5dd40c8
Jashay Lilani <jashay.lilani@gmail.com>
2021-09-18 18:02:53 -0400

    Subarray sum equals K

---
+++ SubarraySumEqualsK.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class SubarraySumEqualsK {
+  public int subarraySum(int[] nums, int k) {
+    int currSum = 0;
+    Map<Integer, Integer> map = new HashMap();
+    int ans = 0;
+    map.put(0, 1);
+    
+    for (int x : nums) {
+        currSum += x;
+        int diff = currSum-k;
+        
+        if (map.containsKey(diff)) {
+            ans += map.get(diff);
+        }
+        
+        map.put(currSum, map.getOrDefault(currSum, 0)+1);
+    }
+    
+    return ans;
+  }
+}
