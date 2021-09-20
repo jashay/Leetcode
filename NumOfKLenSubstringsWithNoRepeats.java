c12cc1d1d8974e46ef711ef537b9d51d85d17bf8
Jashay Lilani <jashay.lilani@gmail.com>
2021-09-20 18:19:22 -0400

    Number of K len substrings with no repeats

---
+++ NumOfKLenSubstringsWithNoRepeats.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class NumOfKLenSubstringsWithNoRepeats {
+  public int numKLenSubstrNoRepeats(String s, int k) {
+    int end = 0, start = 0;
+    int count=0;
+    if (k>s.length()) return 0;
+    
+    Map<Character,Integer> map = new HashMap();
+    char temp;
+    int dups = 0;
+    for (end=0; end<s.length(); end++) {
+        
+        if (end-start >= k) {
+            temp = s.charAt(start);
+            map.put(temp, map.get(temp)-1);
+            if (map.get(temp) > 0)
+                dups--;
+            if (map.get(temp) == 0) map.remove(temp);
+            start++;
+        }
+        temp = s.charAt(end);
+        if (map.containsKey(temp)) {
+            dups++;
+        }
+        map.put(temp, map.getOrDefault(temp, 0)+1);
+        
+        if (dups == 0 && end-start+1 == k) count++;
+    }
+    
+    
+    
+    return count;
+}
+}
