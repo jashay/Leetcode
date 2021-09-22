b7a874d91e979e059180f0a6fc4a693b5c44bcd6
Jashay Lilani <jashay.lilani@gmail.com>
2021-09-22 12:26:19 -0400

    Shortest Word Distance

---
+++ ShortestWordDistance.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class ShortestWordDistance {
+  public int shortestDistance(String[] wordsDict, String word1, String word2) {
+    Map<String, List<Integer>> map = new HashMap();
+    
+    for (int i=0; i<wordsDict.length; i++) {
+        String s = wordsDict[i];
+        List<Integer> list;
+        if (map.containsKey(s)) {
+            list = map.get(s);
+        }
+        else {
+            list = new ArrayList();
+        }
+        list.add(i);
+        map.put(s, list);
+    }
+    
+    List<Integer> loc1 = map.get(word1);
+    List<Integer> loc2 = map.get(word2);
+    int l1 = 0, l2 = 0;
+    int min = Integer.MAX_VALUE;
+    while (l1<loc1.size() && l2<loc2.size()) {
+        min = Math.min(min, Math.abs(loc1.get(l1) - loc2.get(l2)));
+    
+        if (loc1.get(l1) < loc2.get(l2)) {
+            l1++;
+        }
+        else {
+            l2++;
+        }
+    }
+    
+    return min;
+}
+}
