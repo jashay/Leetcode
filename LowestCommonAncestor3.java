e21bb7d94f4832d3f5ff0ec02d77467a30eb601c
Jashay Lilani <jashay.lilani@gmail.com>
2021-09-22 23:58:19 -0400

    Lowest common ancestor 3

---
+++ LowestCommonAncestor3.java
@@ -0,0 +1,13 @@
+public class LowestCommonAncestor3 {
+  // public Node lowestCommonAncestor(Node p, Node q) {
+  //   Node x = p;
+  //   Node y = q;
+    
+  //   while (x != y) {
+  //       x = x==null ? q : x.parent;
+  //       y = y==null ? p : y.parent;
+  //   }
+    
+  //   return x;
+  // }
+}
