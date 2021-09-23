410207aa7e9943d1681c9755dfca0d8a9e316c49
Jashay Lilani <jashay.lilani@gmail.com>
2021-09-23 15:38:11 -0400

    Longest word

---
+++ LongestWord.java
@@ -0,0 +1,17 @@
+public class LongestWord {
+  public String longestWord(String[] words) {
+    Set<String> set = new HashSet<>();
+    String maxWord = "";
+    Arrays.sort(words);
+    
+    for (String word : words) {
+        if (word.length() == 1 || set.contains(word.substring(0, word.length()-1))) {
+            set.add(word);
+            maxWord = maxWord.length() < word.length() ? word : maxWord;
+        }
+    }
+    
+    
+    return maxWord;
+}
+}
