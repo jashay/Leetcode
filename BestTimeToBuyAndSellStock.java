603334b7b71bf32e23f4758c743e119791fb7dcc
Jashay Lilani <jashay.lilani@gmail.com>
2021-09-29 13:30:31 -0400

    Best time to buy and sell stock

---
+++ BestTimeToBuyAndSellStock.java
@@ -0,0 +1,13 @@
+public class BestTimeToBuyAndSellStock {
+  public int maxProfit(int[] prices) {
+    int minprice = Integer.MAX_VALUE;
+    int maxProfit = 0;
+    
+    for (int i=0; i<prices.length; i++) {
+        if (prices[i] < minprice) minprice = prices[i];
+        else if (prices[i]-minprice > maxProfit) maxProfit = prices[i]-minprice;
+    }
+    
+    return maxProfit;
+  }
+}
