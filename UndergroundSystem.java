90e2a36156cfcddc3603f40e361551027b6ecd72
Jashay Lilani <jashay.lilani@gmail.com>
2021-10-02 16:43:45 -0400

    Underground System

--- UndergroundSystem.java
+++ UndergroundSystem.java
@@ -1,7 +1,19 @@
 import java.util.*;
+
 class UndergroundSystem {
 
-  Map<Integer, Pair<String, Integer>> customerCheckInMap;
+  class Pair {
+    String key;
+    int value;
+
+    Pair(String m, int v) {
+      this.key = m;
+      this.value = v;
+    }
+    
+  }
+
+  Map<Integer, Pair> customerCheckInMap;
   Map<String, List<Integer>> timeMap;
   
   public UndergroundSystem() {
@@ -10,20 +22,20 @@ class UndergroundSystem {
   }
   
   public void checkIn(int id, String stationName, int t) {
-      customerCheckInMap.put(id, new Pair<String, Integer> (stationName, t));
+      customerCheckInMap.put(id, new Pair(stationName, t));
   }
   
   public void checkOut(int id, String stationName, int t) {
       if (customerCheckInMap.containsKey(id)) {
-          int checkInTime = customerCheckInMap.get(id).getValue();
+          int checkInTime = customerCheckInMap.get(id).value;
           List<Integer> list;
-          if (timeMap.containsKey(customerCheckInMap.get(id).getKey()+"_"+stationName)) {
-              list = timeMap.get(customerCheckInMap.get(id).getKey()+"_"+stationName);
+          if (timeMap.containsKey(customerCheckInMap.get(id).key+"_"+stationName)) {
+              list = timeMap.get(customerCheckInMap.get(id).key+"_"+stationName);
           }
           else list = new ArrayList();
           
           list.add(t-checkInTime);
-          timeMap.put(customerCheckInMap.get(id).getKey()+"_"+stationName, list);
+          timeMap.put(customerCheckInMap.get(id).key+"_"+stationName, list);
       }
   }
   
