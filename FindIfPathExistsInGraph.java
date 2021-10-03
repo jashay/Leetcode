fc076dd98ffefc4c6e6b51e27d10cc36cc69c398
Jashay Lilani <jashay.lilani@gmail.com>
2021-10-03 16:08:16 -0400

    Find if path is in graph

---
+++ FindIfPathExistsInGraph.java
@@ -0,0 +1,31 @@
+class FindIfPathExistsInGraph {
+  public boolean validPath(int n, int[][] edges, int start, int end) {
+    Set<Integer> visited = new HashSet();
+    Map<Integer, List<Integer>> map = new HashMap();
+    for (int[] edge : edges) {
+        map.putIfAbsent(edge[0], new ArrayList());
+        map.get(edge[0]).add(edge[1]);
+        map.putIfAbsent(edge[1], new ArrayList());
+        map.get(edge[1]).add(edge[0]);
+    }
+    
+    Queue<Integer> q = new LinkedList();
+    q.add(start);
+    
+    while (!q.isEmpty()) {
+        int curr = q.poll();
+        if (visited.contains(curr)) continue;
+        
+        if (curr == end) return true;
+        visited.add(curr);
+        
+        if (map.containsKey(curr)) {
+            for (int nei : map.get(curr)) {
+                q.add(nei);
+            }
+        }
+    }
+    
+    return false;
+}
+}
