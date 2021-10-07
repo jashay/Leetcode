94e2bdacd77489923ce9d621a32b0971329e3eff
Jashay Lilani <jashay.lilani@gmail.com>
2021-10-07 15:39:56 -0400

    Continuous subarray sum multiple of K

---
+++ ContinuousSubarraySum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class ContinuousSubarraySum {
+  public boolean checkSubarraySum(int[] nums, int k) {
+        //USing modular logic
+        
+        Map<Integer, Integer> map = new HashMap();
+        int sum = 0;
+        map.put(0, -1);
+        
+        for (int i=0; i<nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum % k)) {
+                if (i-map.get(sum %k) >=2) return true;
+                else continue;
+            }
+            else {
+                map.put(sum%k, i);
+            }
+        }
+    
+        return false;
+    }
+}
