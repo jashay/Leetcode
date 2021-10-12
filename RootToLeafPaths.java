7c17c368bafcb8b04507e76063a95405036fd91b
Jashay Lilani <jashay.lilani@gmail.com>
2021-10-12 11:04:27 -0400

    Root to leaf Paths

---
+++ RootToLeafPaths.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class RootToLeafPaths {
+  List<String> ans = new ArrayList();
+    
+    public void dfs(TreeNode root, String currPath) {
+        if (root == null) return;
+        
+        if (root.left == null && root.right == null) {
+            currPath += root.val;
+            ans.add(currPath);
+            return;
+        }
+        
+        if (root.left != null) dfs(root.left, currPath+root.val+"->");
+        if (root.right != null) dfs(root.right, currPath+root.val+"->");
+        return;
+    }
+    
+    
+    public List<String> binaryTreePaths(TreeNode root) {
+        dfs(root, "");
+        
+        return ans;
+    }
+}
