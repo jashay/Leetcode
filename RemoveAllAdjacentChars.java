a5bb568a3dd8925fd3d01fb131f017576ef79ad3
Jashay Lilani <jashay.lilani@gmail.com>
2021-10-13 00:33:03 -0400

    Remove all adjacent chars

---
+++ RemoveAllAdjacentChars.java
@@ -0,0 +1,21 @@
+public class RemoveAllAdjacentChars {
+  public String removeDuplicates(String S) {
+    Stack<Character> stack = new Stack();
+    
+    for (char c : S.toCharArray()) {
+        if (!stack.isEmpty() && stack.peek() == c) {
+            stack.pop();
+        }
+        else {
+            stack.push(c);
+        }
+    }
+    
+    StringBuilder sb = new StringBuilder();
+    
+    while (!stack.isEmpty()) sb.append(stack.pop());
+    
+    
+    return sb.reverse().toString();
+}
+}
