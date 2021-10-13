ecbca1b111936f6df5fb78b74db75c1f40a08974
Jashay Lilani <jashay.lilani@gmail.com>
2021-10-13 12:19:12 -0400

    Restore IP address

---
+++ RestoreIPAddress.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class RestoreIPAddress {
+  List<String> ans = new ArrayList();
+    
+    public void backtrack(String s, String ip, int i, int count) {
+        if (count>4) return;
+        if (count == 4 && i==s.length()) {
+            ans.add(ip);
+            return;
+        }
+            
+        for (int x=1; x<4; x++) {
+            if (x+i>s.length()) break;
+            String str = s.substring(i, x+i);
+            if ((str.startsWith("0") && str.length() >1) || (x==3 && Integer.parseInt(str)>255)) continue;
+            backtrack(s, ip+str+(count==3 ? "" : "."), i+x, count+1);
+        }
+    }
+    
+    
+    public List<String> restoreIpAddresses(String s) {
+        
+        backtrack(s, "", 0, 0);
+        return ans;
+    }
+}
