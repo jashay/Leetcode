216e6f38fea5562a0492bc13c32a97d21e61cfa8
Jashay Lilani <jashay.lilani@gmail.com>
2021-10-18 16:14:33 -0400

    Random Pick Index

---
+++ RandomPickIndex.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class RandomPickIndex {
+  Map<Integer, List<Integer>> map = new HashMap();
+  public RandomPickIndex(int[] nums) {
+      for (int i=0; i<nums.length; i++) {
+          int num = nums[i];
+          map.putIfAbsent(num, new ArrayList());
+          map.get(num).add(i);
+      }
+  }
+  
+  public int pick(int target) {
+      if (!map.containsKey(target)) return -1;
+      
+      Random rand = new Random();
+      List<Integer> indexes = map.get(target);
+      return indexes.get(rand.nextInt(indexes.size()));
+  }
+}
