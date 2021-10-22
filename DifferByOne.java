b91006cae27017698bff1a778f6fcd8be6ca140f
Jashay Lilani <jashay.lilani@gmail.com>
2021-10-22 16:03:19 -0400

    Differ by one

---
+++ DifferByOne.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class DifferByOne {
+  public boolean differByOne(String[] dict) {
+    Set<String> set = new HashSet();
+    
+    for (String w : dict) {
+        for (int i=0; i<w.length(); i++) {
+            String sub = ((i==0) ? "*" : w.substring(0, i)) + "*"+w.substring(i+1, w.length());
+            if (set.contains(sub)) return true;
+            set.add(sub);
+        }
+    }
+    
+    return false;
+}
+}
