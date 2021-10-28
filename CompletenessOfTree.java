8c2113e194d9ec1b82e033c530e88032fd257eb7
Jashay Lilani <jashay.lilani@gmail.com>
2021-10-28 10:46:47 -0400

    Completeness of Tree

---
+++ CompletenessOfTree.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class CompletenessOfTree {
+  public boolean isCompleteTree(TreeNode root) {
+    if (root == null) return true;
+    
+    Queue<TreeNode> q = new LinkedList();
+    q.add(root);
+    boolean end = false;
+    
+    while (!q.isEmpty()) {
+        int size = q.size();
+        for (int i=0; i<size; i++) {
+            TreeNode curr = q.poll();
+            if (curr == null) {
+                end = true;
+            }
+            else {
+                if (end) return false;
+                q.add(curr.left);
+                q.add(curr.right);  
+            }
+             
+        }
+
+        
+    }
+    
+    return true;
+}
+}
