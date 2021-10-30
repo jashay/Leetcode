69c47635815a0f9301e60b733eb30ede2e4a141a
Jashay Lilani <jashay.lilani@gmail.com>
2021-10-30 12:45:28 -0400

    Serialize BTree

---
+++ Codec.java
@@ -0,0 +1,49 @@
+import java.util.*;
+class Codec {    
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList();
+        q.add(root);
+        StringBuilder sb = new StringBuilder();
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i=0; i<size; i++) {
+                TreeNode curr = q.poll();
+                
+                if (curr == null) {
+                    sb.append("null,");
+                    continue;
+                }
+                sb.append(curr.val+",");
+                q.add(curr.left);
+                q.add(curr.right);
+            }
+        }
+        return sb.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] arr = data.split(",");
+        if (arr.length ==0 || arr[0].equals("null")) return null;
+        TreeNode root = new TreeNode(Integer.parseInt(arr[0]));
+        int i =1;
+        Queue<TreeNode> q=  new LinkedList();
+        q.add(root);
+        while (!q.isEmpty() && i < arr.length) {
+            TreeNode node = q.poll();
+            if (!arr[i].equals("null")) {
+                node.left = new TreeNode(Integer.parseInt(arr[i]));
+                q.add(node.left);
+            }
+            i++;
+            if (i < arr.length && !arr[i].equals("null")) {
+                node.right = new TreeNode(Integer.parseInt(arr[i]));
+                q.add(node.right);
+            }
+            i++;
+        }
+    
+        return root;
+    }
+}
