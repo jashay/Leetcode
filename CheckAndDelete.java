a2da950c5493d655b91eac75a97ed9e37d66708f
Jashay Lilani <jashay.lilani@gmail.com>
2021-11-04 10:05:22 -0400

    Check And Delete

---
+++ CheckAndDelete.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class CheckAndDelete {
+  Set<Integer> set = new HashSet();
+  List<TreeNode> ans = new ArrayList();
+    
+  public TreeNode check(TreeNode root, boolean is_root) {
+      if (root == null) return null;
+      
+      boolean deleted = set.contains(root.val);
+      
+      if (is_root && !deleted) ans.add(root);
+      
+      root.left = check(root.left, deleted);
+      root.right = check(root.right, deleted);
+      
+      return deleted ? null : root;
+  }
+    
+  public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+      
+      for (int x : to_delete) {
+          set.add(x);
+      }
+      
+      check(root, true);
+      
+      return ans;
+  }
+}
