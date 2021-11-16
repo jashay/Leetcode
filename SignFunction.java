dd77fa3fe14f766d0b7f5723d7d1c26de15f062f
Jashay Lilani <jashay.lilani@gmail.com>
2021-11-16 11:29:47 -0500

    signed function

---
+++ SignFunction.java
@@ -0,0 +1,11 @@
+public class SignFunction {
+  public int arraySign(int[] nums) {
+    int count = 0;
+    for (int x : nums) {
+        if (x == 0) return 0;
+        else if (x < 0) count++;
+    }
+    
+    return count %2 == 0 ? 1 : -1;
+  }
+}
