bf9005f8036519a915ff1388e6416e0f3ccc06a6
Jashay Lilani <jashay.lilani@gmail.com>
2021-11-16 11:46:02 -0500

    Min deletions to make unique

---
+++ MinDeletionsToUnique.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class MinDeletionsToUnique {
+  public int minDeletions(String s) {
+        
+    int[] count = new int[26];
+    for (char c : s.toCharArray()) {
+        count[c-'a']++;
+    }
+    
+    Arrays.sort(count);
+    int ans =0;
+    Set<Integer> set = new HashSet();
+    for (int i= count.length-1; i>=0; i--) {
+        while (count[i] != 0 && set.contains(count[i])) {
+            count[i]--;
+            ans++;
+        }
+        if (count[i] != 0) set.add(count[i]);
+    }
+    
+    return ans;
+  }
+}
