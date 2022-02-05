1115c66c9cdf8f236fa466f934fcf0c5d1d5fd19
jashay <jashay.lilani@gmail.com>
2022-02-04 19:26:06 -0500

    Word Pattern

---
+++ WordPattern.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        Map<Character, Integer> map = new HashMap();
+        int index = 0;
+        String pat = "";
+        for (char c : pattern.toCharArray()) {
+            if (map.containsKey(c)) {
+                pat += map.get(c);
+            }
+            else {
+                index++;
+                map.put(c, index);
+                pat += index;
+            }
+        }
+        
+        String[] arr = s.split(" ");
+        Map<String, Integer> m = new HashMap();
+        index = 0;
+        String s2 = "";
+        
+        for (String str : arr) {
+            if (m.containsKey(str)) {
+                s2 += m.get(str);
+            }
+            else {
+                index++;
+                m.put(str, index);
+                s2 += index;
+            }
+        }
+        
+        
+        return s2.equals(pat);
+    }
+}
