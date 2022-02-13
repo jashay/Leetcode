3c741261dddcb7734f3933476254897b6a9a1869
jashay <jashay.lilani@gmail.com>
2022-02-12 20:29:19 -0500

    Rotate Box

--- RotateBox.java
+++ RotateBox.java
@@ -3,32 +3,32 @@
 class RotateBox {
 	
 	public char[][] rotateTheBox(char[][] box) {
-        	int rows = box.length;
-        	int cols = box[0].length;
-        
-        	char[][] ans = new char[cols][rows];
-        
-        	for (int i=0; i<box.length; i++) {
-            		int lastPos = box[0].length-1;
-            		for (int j=box[0].length-1; j>=0; j--) {
-                		if (box[i][j] == '*') {
-                    			lastPos = j-1;
-                		}
-                		else if (box[i][j] == '#') {
-                    			box[i][j] = '.';
-                   			 box[i][lastPos--]='#';
-               			 }
-           		 }
-       		 }
-        
-        	for (int j=0; j<cols; j++) {
-            		for (int i=rows-1; i>=0; i--) {
-                		ans[j][rows-1-i] = box[i][j];
-           		 }
-       		 }
-        
-       		 return ans;
-    	}	
+		int rows = box.length;
+		int cols = box[0].length;
+	
+		char[][] ans = new char[cols][rows];
+	
+		for (int i=0; i<box.length; i++) {
+			int lastPos = box[0].length-1;
+			for (int j=box[0].length-1; j>=0; j--) {
+				if (box[i][j] == '*') {
+					lastPos = j-1;
+				}
+				else if (box[i][j] == '#') {
+					box[i][j] = '.';
+					box[i][lastPos--]='#';
+				}
+			}
+		}
+	
+		for (int j=0; j<cols; j++) {
+			for (int i=rows-1; i>=0; i--) {
+				ans[j][rows-1-i] = box[i][j];
+			}
+		}
+	
+		return ans;
+    }	
 
 }
 
