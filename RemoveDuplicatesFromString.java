6fb4ee7dd25ea07dcfbc07f29860cedaf7c55ac2
jashay <jashay.lilani@gmail.com>
2022-03-26 13:38:58 -0400

    fixes

--- RemoveDuplicatesFromString.java
+++ RemoveDuplicatesFromString.java
@@ -9,31 +9,31 @@ public class RemoveDuplicatesFromString {
     }
 }
 
-public String removeDuplicates(String s, int k) {
-    Stack<Pair> stack = new Stack();
-    for (int i=0; i<s.length(); i++) {
-        char c = s.charAt(i);
-        if (!stack.isEmpty() && stack.peek().ch == c) {
-            if (stack.peek().count == k-1) {
-                stack.pop();
+    public String removeDuplicates(String s, int k) {
+        Stack<Pair> stack = new Stack();
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if (!stack.isEmpty() && stack.peek().ch == c) {
+                if (stack.peek().count == k-1) {
+                    stack.pop();
+                }
+                else {
+                    Pair p = stack.pop();
+                    stack.push(new Pair(c, p.count+1));
+                }
             }
             else {
-                Pair p = stack.pop();
-                stack.push(new Pair(c, p.count+1));
+                stack.push(new Pair(c, 1));
             }
         }
-        else {
-            stack.push(new Pair(c, 1));
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while (!stack.isEmpty()) {
+            Pair p = stack.pop();
+            for (int i=0; i<p.count; i++) sb.append(p.ch);
         }
+        
+        return sb.reverse().toString(); 
     }
-    
-    StringBuilder sb = new StringBuilder();
-    
-    while (!stack.isEmpty()) {
-        Pair p = stack.pop();
-        for (int i=0; i<p.count; i++) sb.append(p.ch);
-    }
-    
-    return sb.reverse().toString(); 
-}
 }
