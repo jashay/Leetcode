4f0c07752d5c9116ff585f76fbfbe74d820a9f35
jashay <jashay.lilani@gmail.com>
2022-03-27 22:03:02 -0400

    fixes

--- RestoreArray.java
+++ RestoreArray.java
@@ -1,6 +1,7 @@
+import java.util.*;
 class RestoreArray {
-    Set<Integer> visited = new HashSet();
-    List<Integer> list = new ArrayList();
+    Set<Integer> visited = new HashSet<>();
+    List<Integer> list = new ArrayList<>();
     
     public void dfs(int node, Map<Integer, List<Integer>> map) {
         if (visited.contains(node)) return;
@@ -15,8 +16,8 @@ class RestoreArray {
     }
     
     public int[] restoreArray(int[][] adjacentPairs) {
-        Map<Integer, List<Integer>> map = new HashMap();
-        Set<Integer> set = new HashSet();
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
         for (int[] p : adjacentPairs) {
             int x = p[0];
             int y = p[1];
@@ -26,14 +27,14 @@ class RestoreArray {
             if (set.contains(y)) set.remove(y);
             else set.add(y);
             
-            map.putIfAbsent(x, new ArrayList());
-            map.putIfAbsent(y, new ArrayList());
+            map.putIfAbsent(x, new ArrayList<>());
+            map.putIfAbsent(y, new ArrayList<>());
             
             map.get(x).add(y);
             map.get(y).add(x);
         }
         
-        List<Integer> l = new ArrayList(set);
+        List<Integer> l = new ArrayList<>(set);
         int start = l.get(0);
         dfs(start, map);
         int[] ans = new int[list.size()];
